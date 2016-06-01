a68c52480358caccc9f20319cbb47a4d64c0b75e
wuyunjian <wuyunjian.pt@gmail.com>
2016-06-02 00:28:08 +0800

    change main activity

--- app/src/main/java/com/demo/wyj/hello/MainActivity.java
+++ app/src/main/java/com/demo/wyj/hello/MainActivity.java
@@ -2,12 +2,21 @@ package com.demo.wyj.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+    }
+
+    private void test(){
+
     }
 }
